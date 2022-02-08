4bca3892c2446d7d7a490eb8e3f15ab02f151c6e
Sahar Omar <soa7961@rit.edu>
2022-02-08 20:33:47 +0300

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.get('/', (req, res ) =>
+    res.json("Our Container is Running!")
+);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
